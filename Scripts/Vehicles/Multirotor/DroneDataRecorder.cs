50bdf4a3a0e7f8e89534ff4b08eceac1c12049d7
satofumi <satofumin@gmail.com>
2023-03-06 02:40:04 +0900

    feat: add drone data recorder

---
+++ Scripts/Vehicles/Multirotor/DroneDataRecorder.cs
@@ -0,0 +1,71 @@
+using Codice.Client.Common;
+using Sirenix.Serialization;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DroneDataRecorder : MonoBehaviour
+{
+    [SerializeField]
+    private bool _recording;
+
+    [SerializeField]
+    private string _filePath;
+    [SerializeField]
+    private float _recordInterval;
+
+    private float _totalElapsed;
+    private StreamWriter _writer;
+    private float _elapsed;
+
+    private const string FLOAT_FORMAT = "0.0000";
+
+
+    private void Start()
+    {
+        if (_recording)
+        {
+            Record();
+        }
+    }
+    private void OnApplicationQuit()
+    {
+        Stop();
+    }
+
+    public void Record()
+    {
+        _elapsed = 0;
+        _writer = new StreamWriter(File.Open(_filePath, FileMode.OpenOrCreate, FileAccess.Write));
+        _recording = true;
+    }
+    public void Stop()
+    {
+        if (_writer != null)
+        {
+            _writer.Close();
+            _writer = null;
+            _recording = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        var deltaTime = UnityEngine.Time.deltaTime;
+        _totalElapsed += deltaTime;
+
+        if (_recording)
+        {
+            _elapsed += deltaTime;
+            if (_elapsed > _recordInterval)
+            {
+                _elapsed -= _recordInterval;
+
+                var position = transform.position;
+                var angles = transform.eulerAngles;
+                _writer.WriteLine($"{_totalElapsed}\t{position.x.ToString(FLOAT_FORMAT)}\t{position.y.ToString(FLOAT_FORMAT)}\t{position.z.ToString(FLOAT_FORMAT)}\t{angles.x.ToString(FLOAT_FORMAT)}\t{angles.y.ToString(FLOAT_FORMAT)}\t{angles.z.ToString(FLOAT_FORMAT)}");
+            }
+        }
+    }
+}
