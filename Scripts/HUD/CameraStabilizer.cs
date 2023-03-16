4630c839c29e94939d5ceeb54a637e621ce432f9
satofumi <satofumin@gmail.com>
2023-03-17 02:02:23 +0900

    fix: camera stabilizer

--- Scripts/HUD/CameraStabilizer.cs
+++ Scripts/HUD/CameraStabilizer.cs
@@ -1,25 +1,21 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class CameraStabilizer : MonoBehaviour
 {
-    private  List<float> _rollDegrees = new();
-    private float _rollAveragePeriod;
+    private List<float> _rollDegrees = new();
 
     private void Update()
     {
         var angles = transform.eulerAngles;
 
         _rollDegrees.Add((angles.z > 180.0f) ? angles.z - 360.0f : angles.z);
-        _rollAveragePeriod += Time.deltaTime;
-        if (_rollAveragePeriod > 0.15f)
+        if (_rollDegrees.Count > 5)
         {
             _rollDegrees.RemoveAt(0);
         }
-        var averageZ = _rollDegrees.Sum() / _rollDegrees.Count();
+        var averageZ = _rollDegrees.Sum() / _rollDegrees.Count;
 
         transform.eulerAngles = new Vector3(0, angles.y, averageZ);
     }
